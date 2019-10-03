77d28e01150d92b8dffff4f4b054a19c0581b0a3
Alex Kovach <alexander.kovalchuk.aka.maki@gmail.com>
2019-10-03 10:18:34 +0300

    JS-1 второй вариант реализации + комментарии

--- javascript-1(sum).js
+++ javascript-1(sum).js
@@ -1,4 +1,26 @@
+/**
+ * ..последовательный вызов функции
+ * sum()
+ * sum()
+ * sum()
+ */
 var sum = (function(){
 	var ret = 0;
 	return function(n) { return n == undefined ? ret : ret += n; }
 })();
+
+/**
+ * ..вызов функции в одну строку - возвращает, либо себя, либо результат суммирования
+ * sum()()()()()()()
+ */
+var sum = (function(){
+	var ret = 0;
+	var f = function (n){
+		if (n==undefined){
+			return ret;
+        }else{
+			ret+=n;
+			return f
+		};};
+	return f;
+})()
