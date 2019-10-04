57bcc54e6e1cbd84c0d0a9632bb1d4b4787d709b
Alex Kovach <alexander.kovalchuk.aka.maki@gmail.com>
2019-10-04 09:03:43 +0300

    JS-1 добавлен сброс агрегатора при повторном использовании функции sum()()()..

--- javascript-1(sum).js
+++ javascript-1(sum).js
@@ -1,25 +1,47 @@
 /**
  * ..последовательный вызов функции
- * sum()
- * sum()
- * sum()
+ * sumNext()
+ * sumNext()
+ * sumNext()
  */
-var sum = (function(){
+var sumNext = (function(){
 	var ret = 0;
 	return function(n) { return n == undefined ? ret : ret += n; }
 })();
 
 /**
  * ..вызов функции в одну строку - возвращает, либо себя, либо результат суммирования
- * sum()()()()()()()
+ * sumAll()()()()()()()
  */
-var sum = (function(){
+var sumAll = (function(){
 	var ret = 0;
 	var f = function (n){
 		if (n==undefined){
 			return ret;
         }else{
 			ret+=n;
+			return f
+		};};
+	return f;
+})()
+
+/**
+ * ..вызов функции в одну строку - возвращает, либо себя, либо результат суммирования
+ * sumAllWithClear()()()()()()()
+ *
+ *    * добавлен сброс агрегатора в случае нового использования функции
+ *
+ */
+var sumAllWithClear = (function(){
+	var ret = 0;
+	var retVal = 0;
+	var f = function (n){
+		if (n===undefined){
+			retVal = ret;
+			ret = 0;
+			return retVal;
+        }else{
+			ret += n;
 			return f
 		};};
 	return f;
